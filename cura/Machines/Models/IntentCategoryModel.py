8cb38340b69b56d37ca630885c5932c08ad0cba0
Vitaly <random.request.h@gmail.com>
2022-12-15 15:02:24 +0200

    add hightemp intent profiles

--- cura/Machines/Models/IntentCategoryModel.py
+++ cura/Machines/Models/IntentCategoryModel.py
@@ -54,6 +54,11 @@ class IntentCategoryModel(ListModel):
                 "name": catalog.i18nc("@label", "Engineering"),
                 "description": catalog.i18nc("@text", "The engineering profile is designed to print functional prototypes and end-use parts with the intent of better accuracy and for closer tolerances.")
             }
+            cls._translations["hightemp"] = {
+                "name": catalog.i18nc("@label", "High-temp"),
+                "description": catalog.i18nc("@text",
+                                             "The high-temp profile is designed to print functional prototypes and end-use parts with the intent of better accuracy, high temperatures, and for closer tolerances.")
+            }
 
         return cls._translations
 
