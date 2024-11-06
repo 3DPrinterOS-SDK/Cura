999594cbbe87acf2977e648fdf87a22a285ef868
Vitaly <vitaly@3dprinteros.com>
2024-11-06 06:51:45 +0200

    update for felix

--- cura/ApplicationMetadata.py
+++ cura/ApplicationMetadata.py
@@ -4,9 +4,9 @@
 # ---------
 # General constants used in Cura
 # ---------
-DEFAULT_CURA_APP_NAME = "layerloop3d-slicer"
-DEFAULT_CURA_DISPLAY_NAME = "LayerLoop Slicer"
-DEFAULT_CURA_VERSION = "5.7.3.0"
+DEFAULT_CURA_APP_NAME = "felix-slicer"
+DEFAULT_CURA_DISPLAY_NAME = "FELIXslicer"
+DEFAULT_CURA_VERSION = "5.8.1.0"
 DEFAULT_CURA_BUILD_TYPE = ""
 DEFAULT_CURA_DEBUG_MODE = False
 DEFAULT_CURA_LATEST_URL = "https://software.ultimaker.com/latest.json"
